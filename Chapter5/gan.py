0c6244e499d0495747dbb0063f6ae6f13bca3e07
Anna Malet <anna.malet@halias.fr>
2025-07-09 16:46:25 +0200

    gan sign sans c fini

--- Chapter5/gan.py
+++ Chapter5/gan.py
@@ -1,23 +1,25 @@
 import torch
 from torch import nn
-import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, sign_dim):
 
         super().__init__()
+        self.sign_dim = sign_dim
         #self.label_embedding = nn.Embedding(classes, classes//2)
         self.model = nn.Sequential(
             #nn.Linear(classes//2 + length, 128),
-            nn.Linear(2, 256),
-            nn.ReLU(),
+            nn.Linear(sign_dim, 256),
+            nn.LeakyReLU(0.2),
             nn.Dropout(0.3),
             nn.Linear(256, 128),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
             nn.Dropout(0.3),
             nn.Linear(128, 64),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
             nn.Dropout(0.3),
             nn.Linear(64, 1),
             nn.Sigmoid(),
@@ -30,7 +32,7 @@ class Discriminator(nn.Module):
 
 class Generator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_dim, sign_dim):
 
         super().__init__()
         #self.label_embedding = nn.Embedding(classes, classes//2)
@@ -38,11 +40,16 @@ class Generator(nn.Module):
         #self.D=D
         self.model = nn.Sequential(
             #nn.Linear(input_dim + classes//2, 64),
-            nn.Linear(2, 16),
-            nn.ReLU(),
-            nn.Linear(16, 32),
-            nn.ReLU(),
-            nn.Linear(32, 2),
+            nn.Linear(input_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.2),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, sign_dim),
         )
 
     def forward(self, x):
@@ -52,24 +59,24 @@ class Generator(nn.Module):
 
     
 class GAN():
-    def __init__(self, epochs, batch_size, num_classes, sign_shape, input_dim, loss_function, lr, log_interval, device="cpu"):
+    def __init__(self, epochs, batch_size, num_classes, sign_dim, input_dim, loss_function, lr_G, lr_D, betas=(0.5, 0.999), device="cpu"):
 
         self.epochs = epochs
         self.batch_size = batch_size
         self.device = device
         self.num_classes = num_classes
         self.input_dim = input_dim
-        self.shape = sign_shape
+        self.sign_dim = sign_dim
         self.loss = loss_function
-        self.generated_data = []
-        self.lr = lr
-        self.log_interval = log_interval
+        self.lr_G = lr_G
+        self.lr_D = lr_D
+        self.betas = betas
 
-        self.generator = Generator().to(device)
-        self.discriminator = Discriminator().to(device)
+        self.generator = Generator(self.input_dim,self.sign_dim).to(device)
+        self.discriminator = Discriminator(self.sign_dim).to(device)
 
-        self.optim_G = torch.optim.Adam(self.generator.parameters(), lr=lr)
-        self.optim_D = torch.optim.Adam(self.discriminator.parameters(), lr=lr)
+        self.optim_G = torch.optim.Adam(self.generator.parameters(), lr=self.lr_G, betas=self.betas)
+        self.optim_D = torch.optim.Adam(self.discriminator.parameters(), lr=self.lr_D, betas=self.betas)
 
     def train_step(self, train_data):
 
@@ -80,12 +87,15 @@ class GAN():
 
         self.generator.train()
         self.discriminator.train()
+
+        losses_G = []
+        losses_D = []
+        
         for epoch in range(self.epochs):
             for n, (real_samples, _) in enumerate(train_loader):
-
                 # Data for training the discriminator
                 real_samples_labels = torch.ones((self.batch_size, 1))
-                latent_space_samples = torch.randn((self.batch_size, 2))
+                latent_space_samples = torch.randn((self.batch_size, self.input_dim))
                 generated_samples = self.generator(latent_space_samples)
                 generated_samples_labels = torch.zeros((self.batch_size, 1))
                 all_samples = torch.cat((real_samples, generated_samples))
@@ -102,7 +112,7 @@ class GAN():
                 self.optim_D.step()
 
                 # Data for training the generator
-                latent_space_samples = torch.randn((self.batch_size, 2))
+                latent_space_samples = torch.randn((self.batch_size, self.input_dim))
 
                 # Training the generator
                 self.generator.zero_grad()
@@ -114,14 +124,23 @@ class GAN():
                 loss_generator.backward()
                 self.optim_G.step()
 
+                #for name, param in self.generator.named_parameters():
+                #    if param.grad is not None:
+                #        print(f"{name} grad mean: {param.grad.mean().item()}")
+        
+        
+            if epoch % 10 == 0:
+                print(f"Epoch: {epoch} Loss D.: {loss_discriminator} Loss G.: {loss_generator}")
+                
+            losses_D.append(loss_discriminator.item())
+            losses_G.append(loss_generator.item())
 
-                # Show loss
-                if epoch % 10 == 0 and n == self.batch_size - 1:
-                    print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
-                    print(f"Epoch: {epoch} Loss G.: {loss_generator}")
+        plt.plot(losses_D, "r.")
+        plt.plot(losses_G, "b.")
+        plt.savefig(f"./modeks_saved/results_sign_sin.png")
+        #with open(f"./resultats_finaux.txt","a") as f:
+            #f.write(f"input_dim:{self.input_dim}, lrG:{self.lr_G}/lrD:{self.lr_D}, size:{self.batch_size} : \nLoss D.: {np.mean(losses_D[100:200])} Loss G.: {np.mean(losses_G[100:200])} \n")
+        plt.show()
+        torch.save(self.generator.state_dict(), "./models_saved/generator_sign_sin.pt")
+        torch.save(self.discriminator.state_dict(), "./models_saved/discriminator_sign_sin.pt")
 
-                for name, param in self.generator.named_parameters():
-                    if param.grad is not None:
-                        print(f"{name} grad mean: {param.grad.mean().item()}")
-
-        torch.save(self.generator.state_dict(), "./models_saved/generator_sin.pt")
--- Chapter5/gan_test.py
+++ Chapter5/gan_test.py
@@ -3,46 +3,84 @@ from torch import nn
 from gan import GAN, Generator
 import math
 import matplotlib.pyplot as plt
-#import iisignature
+import iisignature
 
 # Define variables
 CUDA = False
 batch_size = 32
-lr = 0.001
+lr_G = 0.001
+lr_D = 0.0002
 num_epochs = 300
 loss_function = nn.BCELoss()
 num_classes = 6 # type des données générées
 channels = 2
-gen_size = (channels*(channels**3 -1))/(channels-1)
-input_dim = 100
-log_interval = 2
+gen_size = int((channels*(channels**3 -1))/(channels-1))
+input_dim = 8
+#log_interval = 2
+sig_level = 3
+num_signs = 512
 
-torch.manual_seed(111)
+def create_data():
+    torch.manual_seed(111)
+    data = []
+    for _ in range(num_signs):
+        train_data_length = 1024
+        train_data = torch.zeros((train_data_length, 2))
+        train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
+        train_data[:, 1] = torch.sin(train_data[:, 0])
+        sorted_indices = torch.argsort(train_data[:, 0])
+        train_data = train_data[sorted_indices]
+        # Calculer la signature d'ordre 3
+        signature = torch.from_numpy(iisignature.sig(train_data.numpy(), sig_level)).float()
+        data.append(signature)
+    
+    train_labels = torch.zeros(train_data_length)
+    train_set = [
+        (data[i], train_labels[i]) for i in range(num_signs)
+    ]
+    return train_set
 
-train_data_length = 1024
-train_data = torch.zeros((train_data_length, 2))
-train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
-train_data[:, 1] = torch.sin(train_data[:, 0])
-train_labels = torch.zeros(train_data_length)
-train_set = [
-    (train_data[i], train_labels[i]) for i in range(train_data_length)
-]
+#gan = GAN(num_epochs, batch_size, num_classes, gen_size, input_dim, loss_function, lr_G, lr_D)
+#gan.train_step(train_set)
 
-gan = GAN(num_epochs, batch_size, num_classes, (200,2), input_dim, loss_function, lr, log_interval)
-gan.train_step(train_set)
+#G2 = Generator()
+#G2.load_state_dict(torch.load("./models_saved/generator_sin.pt"))
 
-G2 = Generator()
-G2.load_state_dict(torch.load("./models_saved/generator_sin.pt"))
+def grid_search(train_set):
+    # Learning rates
+    lrG_values = [1e-4, 2e-4, 5e-4, 1e-3]
+    lrD_values = [1e-4, 2e-4, 5e-4, 1e-3]
+    # Betas pour Adam
+    betas_values = [(0.5, 0.999), (0.4, 0.9), (0.0, 0.9)]
+    # Latent space (z) dimension à tester en dernier, d'abord 16 puis 32 puis 64 puis 8
+    latent_dim_values = [8, 16, 32, 64]
+    # Optionnel : batch size
+    batch_size_values = [16, 32, 64, 128]
+    size = 16
+    for dim in latent_dim_values:
+        for beta in betas_values:
+            for lrG in lrG_values:
+                for lrD in lrD_values:
+                    gan = GAN(200, size, num_classes, gen_size, dim, loss_function, lrG, lrD, beta)
+                    gan.train_step(train_set)
 
-latent_space_samples = torch.randn(100, 2)
-generated_samples = G2(latent_space_samples)
-generated_samples = generated_samples.detach()
-plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
-plt.show()
+def grid_search2(train_set):
+    # Learning rates
+    lrG_values = [5e-4, 1e-3]
+    lrD_values = [2e-4, 1e-3]
+    latent_dim_values = [8, 16]
+    batch_size_values = [32, 64, 128]
+    for dim in latent_dim_values:
+        for size in batch_size_values:
+            for lrG in lrG_values:
+                for lrD in lrD_values:
+                    gan = GAN(400, size, num_classes, gen_size, dim, loss_function, lrG, lrD)
+                    gan.train_step(train_set)
 
 
-
-
+if __name__ == "__main__":
+    train_data = create_data()
+    results = grid_search2(train_data)
 
 
 
